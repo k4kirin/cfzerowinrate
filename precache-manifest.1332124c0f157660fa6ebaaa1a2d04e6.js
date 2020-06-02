ef1a4cf996180b07a610332f4b51fdfb1f4ca2bc
Kirin Sarangkasiri <k4kinny@gmail.com>
2020-06-02 23:03:49 +0700

    Updates

---
+++ precache-manifest.1332124c0f157660fa6ebaaa1a2d04e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ca524c50b13c6b17d6240cbf3354b9",
+    "url": "/cfzerowinrate/index.html"
+  },
+  {
+    "revision": "2934e6cde12ce1de5f5e",
+    "url": "/cfzerowinrate/static/css/main.78ea71bf.chunk.css"
+  },
+  {
+    "revision": "e7aa356ce90605dc2701",
+    "url": "/cfzerowinrate/static/js/2.c76405bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cfzerowinrate/static/js/2.c76405bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2934e6cde12ce1de5f5e",
+    "url": "/cfzerowinrate/static/js/main.72a3d354.chunk.js"
+  },
+  {
+    "revision": "a662d1f97a02ace0d930",
+    "url": "/cfzerowinrate/static/js/runtime-main.79581e91.js"
+  }
+]);
