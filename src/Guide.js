532755f0e6373456a4c518dd4c567ab7d9c7c2a7
Kirin Sarangkasiri <k4kinny@gmail.com>
2021-11-23 17:01:25 -0500

    wording

--- src/Guide.js
+++ src/Guide.js
@@ -26,7 +26,8 @@ class Guide extends Component {
 			From here, press the clan you would like to see, then it will tell you your <b>clan level, experience, and wins.</b><br/><br/>
 			Use these three numbers to put into the calculator, and you will get your win rate!<br/><br/>
 			Keep in mind, these will count your matches when you are facing a VP Farmer, and when you are VP Farming, as well.<br/><br/>
-			PS. With the inclusion of tournaments, this calculator will be inaccurate if you've ever used the deck in a tournament. This is because it will not count the win for you on this page, but still get experience. So, if you have ever won with that clan in the tournament mode, the calculation will be skewed into showing that you lost more than you did.<br/><br/>
+			PS. With the inclusion of tournaments, <b>this calculator will be inaccurate if you've ever used the deck in a tournament</b>.<br/><br/>
+			This is because the game will not count wins for you in tournaments, while still getting experience. So, if you have ever won with that clan in the tournament mode, the calculation will be skewed into showing that you lost more than you did.<br/><br/>
 			If you do remember how many times you've won with that clan during tournaments, you should include it as well for accuracy.<br/><br/>
 		</div>
 		);
